da4e90b61e2d8da7fd2c5c1824d32c9ff47fc030
PETERSON\Peterson <peterson@tecbiz.com.br>
2019-08-07 17:55:25 -0300

    ultimas

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+const cors = require('cors');
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// nodeapi = nome da database a ser utilizada
+mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
+requireDir('./src/models');
+
+// Rotas
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello Word! [Ola Mundo]');
-});
-
+app.use('/api', require('./src/routes'));
 app.listen(3001);
